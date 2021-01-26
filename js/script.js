9f94e0617e366d25637fa2331c8f9a5272571410
Nikolay <nn.eliseev@icloud.com>
2021-01-26 19:27:35 +0300

    prepare to task 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let numberOfFilms = askHowManyMoves();
+'use strict';
+let numberOfFilms = askHowManyMoves(),
+    q1 = 'What is the last movie you watched?',
+    q2 = 'What score will you give?';
 
 checkUserStatus(numberOfFilms);
 
@@ -10,13 +13,21 @@ const personalMovieDB = {
     private: false,
 
     //Добавление Фильма и его оценки
-    addNewMovieIntoDB: function (movie, movieScore) {
+    addNewMovieIntoDB: (movie, movieScore) => {
         personalMovieDB.movies[movie] = movieScore;
         //        console.log(personalMovieDB);
     },
     //Вывести все вильмы из базы данных
-    showAllMoviesIntoBD: function () {
+    showAllMoviesIntoBD: () => {
         console.log(personalMovieDB.movies);
+    },
+    //check DB to private if false show in console
+    showMyDB: () => {
+        if (personalMovieDB.private == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log(`DB in private state`);
+        }
     }
 };
 
@@ -27,32 +38,47 @@ for (let i = 0; i < numberOfFilms; i++) {
 }
 
 personalMovieDB.showAllMoviesIntoBD();
-
+personalMovieDB.showMyDB();
 //------------------------FUNCTIONS---------------------------------------///
 //Ask a questions
 //
 // What score will you give?
 // What is the last movie you watched?
 function askQuestionsAboutMovies() {
-    let movie = chekUserUnswer('What is the last movie you watched?'),
-        movieScore = chekUserUnswer('What score will you give?');
+    let movie = chekUserUnswer(q1),
+        movieScore = chekUserUnswer(q2);
 
     personalMovieDB.addNewMovieIntoDB(movie, movieScore);
 }
 
 //Check users unswer (mustn't be empty and more than 50 symbols)
 function chekUserUnswer(question) {
-    let userUnswer = null;
+    
+    let userUnswer = null,
+        startOfAnswer;
+
+    if (question == q1) {
+        startOfAnswer = 'Movie';
+    } else {
+        startOfAnswer = 'Score';
+    }
+
+
     while (userUnswer == null) {
         userUnswer = prompt(question, '');
         if (userUnswer != null) {
             if (userUnswer == '') {
-                alert(`Movie name can't be empty, please try again!`);
-                userUnswer = '';
+                alert(`${startOfAnswer} can't be empty, please try again!`);
+                userUnswer = null;
+            } else if (userUnswer.length > 50) {
+                alert(`${startOfAnswer} length can't be more than 50 symbols, please try again!`);
+                userUnswer = null;
             }
-            if (userUnswer.length > 50) {
-                alert(`Movie length name can't be more than 50 symbols, please try again!`);
-                userUnswer = '';
+            if (startOfAnswer == 'Score') {
+                if (isNaN(userUnswer)) {
+                    alert(`${startOfAnswer} must be a number, please try again!`);
+                    userUnswer = null;
+                }
             }
         } else {
             alertOfCancel();
@@ -71,7 +97,7 @@ function askHowManyMoves() {
             if (userUnswer == '') {
                 alert(`Can't be empty, please try again!`);
                 userUnswer = null;
-            } else if (userUnswer == isNaN) {
+            } else if (isNaN(userUnswer)) {
                 alert(`Unswer must be Number!`);
                 userUnswer = null;
             }
@@ -84,12 +110,13 @@ function askHowManyMoves() {
 
 }
 
+//Give notice for user)
 function checkUserStatus(x) {
-    if (x < 10){
+    if (x < 10) {
         alert(`You whached not so many mivies.`);
-    } else if (x >=10 && x < 30){
+    } else if (x >= 10 && x < 30) {
         alert(`You classic movies whatcher.`);
-    } else if (x >= 30){
+    } else if (x >= 30) {
         alert(`You are big movie fan`);
     }
 }
